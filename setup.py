f7f9a2b5fa0319addea0e3ab18245a35d96757df
lin <lin@n2920>
2026-06-08 12:46:32 +0800

    fix: extend monkey-patch to _check_cuda_version + add CUDA bin to PATH

--- setup.py
+++ setup.py
@@ -91,10 +91,14 @@ ext_modules = []
 cmdclass = {}
 
 if _CUDA_HOME and _GPU_ARCH:
-    # torch.cpp_extension may clear os.environ["CUDA_HOME"] during init.
-    # Monkey-patch _join_cuda_home to use our detected path directly.
+    # torch.cpp_extension functions rely on os.environ which is volatile
+    # in pip build isolation (Python 3.14 / Windows). Patch them directly.
     import torch.utils.cpp_extension as _cpp_ext
     _cpp_ext._join_cuda_home = lambda *paths, _h=_CUDA_HOME: os.path.join(_h, *paths)
+    _cpp_ext._check_cuda_version = lambda *a, **kw: None
+    # Also add CUDA bin to PATH so nvcc is findable during actual compilation
+    _cuda_bin = os.path.join(_CUDA_HOME, "bin")
+    os.environ["PATH"] = _cuda_bin + os.pathsep + os.environ.get("PATH", "")
     _nvcc_args = [
         "-O3",
         "-std=c++17",
